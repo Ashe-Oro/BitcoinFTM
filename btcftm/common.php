fcd8515777274f581cea1a86f89bbf28503117e2
John Zeren <john@johnzeren.com>
2013-11-06 19:37:04 -0700

    Added begining of btce Light coin work.

--- btcftm/common.php
+++ btcftm/common.php
@@ -8,6 +8,7 @@ require_once("private_markets/privatemtgoxusd.php");
 require_once("public_markets/bitstampusd.php");
 require_once("public_markets/mtgoxusd.php");
 require_once("public_markets/bitfinexltc.php");
+require_once("public_markets/btceltc.php");
 
 function iLog($msg)
 {
--- btcftm/public_markets/btceltc.php
+++ btcftm/public_markets/btceltc.php
@@ -1,10 +1,10 @@
 <?php
 require_once("market.php");
 
-class BitfinexLTC extends Market
+class BtceLTC extends Market
 {	
-	private $depthUrl = "https://api.bitfinex.com/v1/book/ltcbtc";
-	private $tickerUrl = "https://api.bitfinex.com/v1/ticker/ltcbtc";
+	private $depthUrl = "https://btc-e.com/api/2/btc_usd/depth";
+	private $tickerUrl = "https://btc-e.com/api/2/btc_usd/ticker";
 
 	public function __construct()
 	{
@@ -15,15 +15,15 @@ class BitfinexLTC extends Market
 
 	public function updateDepth()
 	{
-		iLog("[BitfinexLTC] Updating order depth...");
+		iLog("[BtceLTC] Updating order depth...");
 		$res = file_get_contents($this->depthUrl);
 		try {
 			$json = json_decode($res);
 			$data = $json;
 			$this->depth = $this->formatDepth($data);
-			iLog("[BitfinexLTC] Order depth updated");
+			iLog("[BtceLTC] Order depth updated");
 		} catch (Exception $e) {
-			iLog("[BitfinexLTC] ERROR: can't parse JSON feed - {$url} - ".$e->getMessage());
+			iLog("[BtceLTC] ERROR: can't parse JSON feed - {$url} - ".$e->getMessage());
 		}
 	}
 
@@ -33,7 +33,7 @@ class BitfinexLTC extends Market
 		foreach($l as $i) {
 			array_push($r, array('price' => (float) $i[0], 'amount' => (float) $i[1]));
 		}
-		usort($r, array("BitstampLTC", "comparePrice"));
+		usort($r, array("BtceLTC", "comparePrice"));
 		if ($reverse) {
 			$r = array_reverse($r);
 		}
@@ -49,15 +49,15 @@ class BitfinexLTC extends Market
 
 	public function getCurrentTicker()
 	{
-		iLog("[BitfinexLTC] Getting current ticker...");
+		iLog("[BtceLTC] Getting current ticker...");
 		$res = file_get_contents($this->tickerUrl);
 		try {
 			$json = json_decode($res);
 			$ticker = new Ticker($json);
-			iLog("[BitfinexLTC] Current ticker - mid: {$ticker['mid']} last: {$ticker['last_price']} ask: {$ticker['ask']} bid: {$ticker['bid']}");
+			iLog("[BtceLTC] Current ticker - high: {$ticker['high']} low: {$ticker['low']} avg: {$ticker['avg']} vol: {$ticker['vol']} last: {$ticker['last_price']} sell: {$ticker['sell']} buy: {$ticker['buy']}");
 			return $ticker;
 		} catch (Exception $e) {
-			iLog("[BitfinexLTC] ERROR: can't parse JSON feed - {$this->tickerUrl} - ".$e->getMessage());
+			iLog("[BtceLTC] ERROR: can't parse JSON feed - {$this->tickerUrl} - ".$e->getMessage());
 		}
 	}
 }
