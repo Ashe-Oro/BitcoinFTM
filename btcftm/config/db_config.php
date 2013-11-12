051df0ba66ed88a80c7f829dc3bdf4693829a25d
John Zeren <john@johnzeren.com>
2013-11-11 20:03:31 -0700

    Added if to db config to check for localhost and use local host db if that's where the request is made from.

--- btcftm/config/db_config.php
+++ btcftm/config/db_config.php
@@ -1,10 +1,20 @@
 <?php
 require_once("utils/database_util.php");
 
-$host = 'btcftm.db.7218359.hostedresource.com';
-$user = 'btcftm';
-$pass = 'FTMw1nn1ng!';
-$name = 'btcftm';
+$localhosts = array('127.0.0.1', "::1");
+
+if(!in_array($_SERVER['REMOTE_ADDR'], $localhosts)){
+    $host = 'bbtcftm.db.7218359.hostedresource.com';
+	$user = 'btcftm';
+	$pass = 'FTMw1nn1ng!';
+	$name = 'btcftm';
+}
+else {
+	$host = '127.0.0.1';
+	$user = 'root';
+	$pass = 'root';
+	$name = 'ftm';
+}
 
 $DB = new Database($host, $user, $pass, $name); 
 ?>
