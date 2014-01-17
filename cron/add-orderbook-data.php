cf802c0aeb394318c2daee54e057bf59babbb729
John Zeren <john@johnzeren.com>
2014-01-17 15:35:58 -0700

    UPdated Orderbook cron to cap at 5BTC activity.

--- cron/add-orderbook-data.php
+++ cron/add-orderbook-data.php
@@ -6,25 +6,25 @@ include("../utils/ExchangeDbUtil.php");
 
 $exchangeDb = new ExchangeDbUtil();
 
-$temp = $exchangeDb->addToOrderbooks("mtgox", 10);
+$temp = $exchangeDb->addToOrderbooks("mtgox", 5);
 echo "<br/>MtGox Orderbook Added..." . $temp;
 
-$temp = $exchangeDb->addToOrderbooks("bitstamp", 10);
+$temp = $exchangeDb->addToOrderbooks("bitstamp", 5);
 echo "<br/>Bitstamp Orderbook Added..." . $temp;
 
-$temp = $exchangeDb->addToOrderbooks("btce_btcusd", 10);
+$temp = $exchangeDb->addToOrderbooks("btce_btcusd", 5);
 echo "<br/>Btc-e Orderbook Added...(BTCUSD)" . $temp;
 
-$temp = $exchangeDb->addToOrderbooks("bitfinex_btcusd", 10);
+$temp = $exchangeDb->addToOrderbooks("bitfinex_btcusd", 5);
 echo "<br/>Bitfinex Orderbook Added...(BTCUSD)" . $temp;
 
-$temp = $exchangeDb->addToOrderbooks("kraken_btcusd", 10);
+$temp = $exchangeDb->addToOrderbooks("kraken_btcusd", 5);
 echo "<br/>Kraken Orderbook Added...(BTCUSD)" . $temp;
 
-$temp = $exchangeDb->addToOrderbooks("cryptotrade_btcusd", 10);
+$temp = $exchangeDb->addToOrderbooks("cryptotrade_btcusd", 5);
 echo "<br/>CryptoTrade Orderbook Added...(BTCUSD)" . $temp;
 
-$temp = $exchangeDb->addToOrderbooks("campbx_btcusd", 10);
+$temp = $exchangeDb->addToOrderbooks("campbx_btcusd", 5);
 echo "<br/>CampBX Orderbook Added...(BTCUSD)" . $temp;
 
 ?>
